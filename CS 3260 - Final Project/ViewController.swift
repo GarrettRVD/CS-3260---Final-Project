38f50ea73f57face476e54fbb72de3edf99bfda9
GarrettRVD <garrettvandyke@mail.weber.edu>
2020-08-05 12:21:04 -0600

    Initial Commit

---
+++ CS 3260 - Final Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CS 3260 - Final Project
+//
+//  Created by Garrett R Van Dyke on 8/5/20.
+//  Copyright © 2020 Garrett R Van Dyke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
